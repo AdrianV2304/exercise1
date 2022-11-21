049bd14a9b10999ee136fa3005858447b9b3ffe0
Adi <valcuadrian90@yahoo.com>
2022-11-21 16:42:04 +0200

    mods3

--- exercise1/src/components/cards/Cards.js
+++ exercise1/src/components/cards/Cards.js
@@ -7,27 +7,27 @@ const Cards = () => {
       <div className="collection__cards">
         <img src="https://placeimg.com/640/480/people" alt="random people"/>
         <div className="collection__cards--small">
-          <img src="https://placeimg.com/640/480/people" alt="random people"/>
-          <img src="https://placeimg.com/640/480/people" alt="random people"/>
-          <img src="https://placeimg.com/640/480/people" alt="random people"/>
+          <img src="https://placeimg.com/600/420/people" alt="random people"/>
+          <img src="https://placeimg.com/610/400/people" alt="random people"/>
+          <img src="https://placeimg.com/620/480/people" alt="random people"/>
         </div>
       </div>
 
       <div className="collection__cards">
         <img src="https://placeimg.com/640/480/nature" alt="random nature"/>
         <div className="collection__cards--small">
-          <img src="https://placeimg.com/640/480/nature" alt="random nature"/>
-          <img src="https://placeimg.com/640/480/nature" alt="random nature"/>
-          <img src="https://placeimg.com/640/480/nature" alt="random nature"/>
+          <img src="https://placeimg.com/600/480/nature" alt="random nature"/>
+          <img src="https://placeimg.com/640/400/nature" alt="random nature"/>
+          <img src="https://placeimg.com/610/480/nature" alt="random nature"/>
         </div>
       </div>
 
       <div className="collection__cards">
         <img src="https://placeimg.com/640/480/tech" alt="random tech"/>
         <div className="collection__cards--small">
-          <img src="https://placeimg.com/640/480/tech" alt="random tech"/>
-          <img src="https://placeimg.com/640/480/tech" alt="random tech"/>
-          <img src="https://placeimg.com/640/480/tech" alt="random tech"/>
+          <img src="https://placeimg.com/600/480/tech" alt="random tech"/>
+          <img src="https://placeimg.com/620/400/tech" alt="random tech"/>
+          <img src="https://placeimg.com/610/480/tech" alt="random tech"/>
         </div>
       </div>
 
